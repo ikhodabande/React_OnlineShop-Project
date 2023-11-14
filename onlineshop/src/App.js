275c5b5e28848f7bc9e3d39ab7826ee0eddfa5ad
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-14 20:45:58 +0330

    add routes

--- onlineshop/src/App.js
+++ onlineshop/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import {BrowserRouter as Router , Route , Routes} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <button className="btn btn-success">submit</button>
+      <Router>
+        <Routes>
+          <Route  path="/" />
+        </Routes>
+      </Router>
     </div>
   );
 }
