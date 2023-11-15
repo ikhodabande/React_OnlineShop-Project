2a5024620f3be35a99ee48a768947c3ff272a260
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-15 22:04:22 +0330

    nav up

--- onlineshop/src/components/Navbar.jsx
+++ onlineshop/src/components/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
     <div className="navbar navbar-dark bg-dark navbar-expand-lg">
       <div className="container">
         <a className="navbar-brand">Mehraban SHOP</a>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link to="/" className='nav-link' >shop</Link>
+            </li>
+          <li className="nav-item">
+          <Link to="/cart" className='nav-link' >cart</Link>
+            </li>
+        </ul>
 
       </div>
     </div>
