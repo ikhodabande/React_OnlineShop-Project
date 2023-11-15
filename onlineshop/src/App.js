c5f8cce80ec114f4f126200f47d2a0c44ae8b21a
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-15 21:49:33 +0330

    add shop route

--- onlineshop/src/App.js
+++ onlineshop/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import {BrowserRouter as Router , Route , Routes} from 'react-router-dom'
+import Shop from './pages/shop/Shop';
+import Cart from './pages/cart/Cart';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route  path="/" />
-          <Route  path="/cart" />
+          <Route  path="/" element={Shop}/>
+          <Route  path="/cart" element={Cart} />
         </Routes>
       </Router>
     </div>
