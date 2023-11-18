f8d88b7fe3b1030f70148fdcb5e96179be5b42f3
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-18 11:28:24 +0330

    shopcontextprovider

--- onlineshop/src/context/shopContext.jsx
+++ onlineshop/src/context/shopContext.jsx
@@ -1,1 +1,22 @@
-cosnt shopContext = 
+import { createContext, useState } from "react"
+
+export const shopContext = createContext(null);
+
+export const shopContextProvider = (props) => {
+  const [cartIdtems , setCartItems] = useState([])
+
+
+  const addToCart = () => {
+
+  }
+
+  const removeFromCart = () => {
+
+  }
+
+  const contextValue = {cartIdtems, addToCart, removeFromCart } 
+
+  return <shopContextProvider value={contextValue}>{props.children}</shopContextProvider>
+
+
+}
