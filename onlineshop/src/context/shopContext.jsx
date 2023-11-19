0ed6b6b886b5116bdf1c0ad7a55a5a1304ba9198
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-19 18:42:05 +0330

    remove from cart items added

--- onlineshop/src/context/shopContext.jsx
+++ onlineshop/src/context/shopContext.jsx
@@ -7,14 +7,22 @@ export const ShopContextProvider = (props) => {
 
 
   const addToCart = (itemId) => {
-    setCartItems([...cartItems,{id : itemId , count :1}])
+    if(!cartItems?.find((item)=> item.id === itemId))
+    setCartItems([...cartItems,{id : itemId , count :1}]);
+    else
+    setCartItems(cartItems.map((item)=>{
+     if(item.id === itemId)
+      return {...item, count : item.count + 1};
+    else
+    return item;
+    }))
 
   }
 
   const removeFromCart = (itemId) => {
     setCartItems(cartItems.map((i)=>{
       if(i.id === itemId)
-      return {...i, count : i.count-1}
+      return {...i, count : i.count === 0 ? 0 : i.count-1}
       else return i
     }))
 
