0f60cb700468e794648640cc083502727e62055e
Amirmohammad <amii_mhmd@yahoo.com>
2023-11-29 11:30:04 +0330

    update welcome

--- onlineshop/src/components/welcome.jsx
+++ onlineshop/src/components/welcome.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClose } from '@fortawesome/free-solid-svg-icons'
 
 const Welcome=()=> {
+  const [showWelcome , setShowWelcome] = useState(true)
+  const onHideWelcome=()=>{
+    setShowWelcome(false)
+  }
+
   return (
+    <>
+
+
+    {showWelcome && (
     <div className="container">
       <div className=" bg-primary text-white my-3 ">
          <FontAwesomeIcon
          icon={faClose}
          style={{float:"right", margin:"5px"}}
+         onClick={onHideWelcome}
           />
           <div className="p-4">Welcome</div>
       </div>
     </div>
+
+    )}
+
+    </>
   )
 }
 
